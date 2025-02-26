e32c9567266827062b6420aa13472aa24d30d100
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-26 22:28:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -334,7 +334,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await client.edit_message_media(
             query.message.chat.id, 
             query.message.id, 
-            InputMediaPhoto(random.choice(PICS))
+            InputMediaAnimation("https://media.giphy.com/media/YOUR_GIF_ID/giphy.gif")
         )
         reply_markup = InlineKeyboardMarkup(buttons)
         me2 = (await client.get_me()).mention
